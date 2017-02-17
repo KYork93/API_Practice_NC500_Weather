9db62177e2575908b6abd9264a177ee71b6aa42a
Kristie Yorkston <kyorkston@gmail.com>
2017-02-17 18:23:57 +0000

    add css tags

--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -23,17 +23,17 @@ MapWrapper.prototype = {
     window.setPosition(marker.position);
     google.maps.event.addListener(marker, 'click', function(){
       if(marker.position === info.johnogroats){
-        window.setContent('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/John_O%27Groats_from_the_air.jpg/639px-John_O%27Groats_from_the_air.jpg"><p>The furthest point of the British Isles</p> \n <button onclick="wickCheck()" id="john-weather">Check Weather</button>');
+        window.setContent('<img id="window-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/John_O%27Groats_from_the_air.jpg/639px-John_O%27Groats_from_the_air.jpg"><p>The furthest point of the British Isles</p> \n <button onclick="wickCheck()" id="john-weather">Check Weather</button>');
       } else if (marker.position === info.tongue){
-        window.setContent('<img src="https://upload.wikimedia.org/wikipedia/commons/7/77/The_view_across_Loch_Eriboll_to_Ben_Hope_-_geograph.org.uk_-_234948.jpg"><p>From here you can access Ben Hope</p> \n <button onclick="tongueCheck()" id="tongue-weather">Check Weather</button>')
+        window.setContent('<img id="window-img" src="https://upload.wikimedia.org/wikipedia/commons/7/77/The_view_across_Loch_Eriboll_to_Ben_Hope_-_geograph.org.uk_-_234948.jpg"><p>From here you can access Ben Hope</p> \n <button onclick="tongueCheck()" id="tongue-weather">Check Weather</button>')
       } else if (marker.position === info.ullapool){
-        window.setContent('<img src="http://sealochhouse.co.uk/admin/assets/img/45fb6fef56.jpg"><p>You can catch a ferry from here to get to the beautiful Isle of Skye</p> \n <button onclick="ullapoolCheck()" id="ullapool-weather">Check Weather</button>')
+        window.setContent('<img id="window-img" src="http://sealochhouse.co.uk/admin/assets/img/45fb6fef56.jpg"><p>You can catch a ferry from here to get to the beautiful Isle of Skye</p> \n <button onclick="ullapoolCheck()" id="ullapool-weather">Check Weather</button>')
       } else if (marker.position === info.applecross){
-        window.setContent('<img src="http://www.whatson-north.co.uk/imagelibrary/Client_Images/Client00007/ResizeCache/01988000/01988922%20-%20450x248.jpg"><p>There is a well renowned gastropub here</p> \n <button onclick="appleCheck()" id="apple-weather">Check Weather</button>')
+        window.setContent('<img id="window-img" src="http://www.whatson-north.co.uk/imagelibrary/Client_Images/Client00007/ResizeCache/01988000/01988922%20-%20450x248.jpg"><p>There is a well renowned gastropub here</p> \n <button onclick="appleCheck()" id="apple-weather">Check Weather</button>')
       } else if (marker.position === info.dunrobinCastle){
-        window.setContent('<img src="https://upload.wikimedia.org/wikipedia/commons/6/69/Dunrobin_Castle_-Sutherland_-Scotland-26May2008_(2).jpg"><p>The castle here is features French Renaissance architecture</p> \n <button onclick="castleCheck()" id="castle-weather">Check Weather</button>')
+        window.setContent('<img id="window-img" src="https://upload.wikimedia.org/wikipedia/commons/6/69/Dunrobin_Castle_-Sutherland_-Scotland-26May2008_(2).jpg"><p>The castle here is features French Renaissance architecture</p> \n <button onclick="castleCheck()" id="castle-weather">Check Weather</button>')
       } else if (marker.position === info.inverness){
-        window.setContent('<img src="http://www.flybe.com/cheap-flights/inverness/inverness-overview-16x9.jpg"><p>A growing city in Scotland, the start point and the end point</p> \n <button onclick="invernessCheck()" id="castle-weather">Check Weather</button>')
+        window.setContent('<img id="window-img" src="http://www.flybe.com/cheap-flights/inverness/inverness-overview-16x9.jpg"><p>A growing city in Scotland, the start point and the end point</p> \n <button onclick="invernessCheck()" id="castle-weather">Check Weather</button>')
       }
 
       window.open(this.googleMap, marker);
